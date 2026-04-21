088cb8dbf1b3041e023bc5a9cea3de1cadf21502
Hongjin Li <vic.hong@rock-chips.com>
2026-04-21 17:32:09 +0800

    fix[h264d]: Add missing api call in rkv reg
    Platform: RV1126/RV1109
    Spec: h264
    
    Error case:
    Core dump occurs while decoding the H.264 bitstream.
    
    Solution:
    The call to hal_h264d_explain_input_buffer was lost during the HAL
    API registration refactor. Restore it to ensure input buffer
    info is properly explained before generating registers.
    
    Change-Id: Ic290c096d7b4b66692d7fc2680c9954330b42b3c
    Signed-off-by: Hongjin Li <vic.hong@rock-chips.com>

--- mpp/hal/rkdec/h264d/hal_h264d_rkv_reg.c
+++ mpp/hal/rkdec/h264d/hal_h264d_rkv_reg.c
@@ -473,6 +473,7 @@ MPP_RET rkv_h264d_gen_regs(void *hal, HalTaskInfo *task)
         task->dec.reg_index = 0;
     }
 
+    hal_h264d_explain_input_buffer(hal, &task->dec);
     prepare_spspps(p_hal, (RK_U64 *)reg_ctx->spspps, sizeof(reg_ctx->spspps));
     prepare_framerps(p_hal, (RK_U64 *)reg_ctx->rps, sizeof(reg_ctx->rps));
     prepare_scanlist(p_hal, (RK_U64 *)reg_ctx->sclst, sizeof(reg_ctx->sclst));
