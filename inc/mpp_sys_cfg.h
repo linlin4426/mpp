e96885ed88f336c7c79b28cacf980c70c838378a
Hongjin Li <vic.hong@rock-chips.com>
2026-06-18 17:52:26 +0800

    refactor[sys_cfg]: Remove unused MppSysDecBufCkhChange enum
    Change-Id: Ieccbb3542af49218a5f0acbc0929f3934d1bad07
    Signed-off-by: Hongjin Li <vic.hong@rock-chips.com>

--- inc/mpp_sys_cfg.h
+++ inc/mpp_sys_cfg.h
@@ -10,29 +10,7 @@
 #include "mpp_list.h"
 #include "mpp_frame.h"
 
-typedef enum MppSysDecBufCkhCfgChange_e {
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_ENABLE           = MPP_BIT(0),
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_TYPE             = MPP_BIT(1),
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_FMT_CODEC        = MPP_BIT(2),
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_FMT_FBC          = MPP_BIT(3),
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_FMT_HDR          = MPP_BIT(4),
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_WIDTH            = MPP_BIT(5),
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_HEIGHT           = MPP_BIT(6),
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_CROP_TOP         = MPP_BIT(7),
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_CROP_BOTTOM      = MPP_BIT(8),
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_CROP_LEFT        = MPP_BIT(9),
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_CROP_RIGHT       = MPP_BIT(10),
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_FLAG_METADATA    = MPP_BIT(11),
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_FLAG_THUMBNAIL   = MPP_BIT(12),
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_H_STRIDE_BYTE    = MPP_BIT(13),
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_V_STRIDE         = MPP_BIT(14),
-
-    MPP_SYS_DEC_BUF_CHK_CFG_CHANGE_ALL              = (0xFFFFFFFFU),
-} MppSysDecBufCkhChange;
-
 typedef struct MppSysBaseCfg_t {
-    RK_U64 change;
-
     RK_U32 enable;
 
     /* input args start */
@@ -82,7 +60,6 @@ typedef struct MppSysBaseCfg_t {
 } MppSysDecBufChkCfg;
 
 typedef struct MppSysCfgSet_t {
-    RK_U32 change;
     MppSysDecBufChkCfg dec_buf_chk;
 } MppSysCfgSet;
 
