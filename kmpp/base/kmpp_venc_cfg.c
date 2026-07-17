e3f6d6799144830f16834cdeb36bccf269e65aba
Herman Chen <herman.chen@rock-chips.com>
2026-07-17 09:48:52 +0800

    feat[kmpp_venc]: Add REF_CFG to kcfg type
    Add MPP_VENC_KCFG_TYPE_REF_CFG for unified ref_cfg lifecycle
    management via mpp_venc_kcfg_init/deinit interface.
    
    Change-Id: Ic7a3e5d9f1b3d5e7a9c1d3e5f7a9b1c3d5e7f9a1
    Signed-off-by: Herman Chen <herman.chen@rock-chips.com>

--- kmpp/base/kmpp_venc_cfg.c
+++ kmpp/base/kmpp_venc_cfg.c
@@ -38,6 +38,7 @@ static char *kcfg_names[] = {
     [MPP_VENC_KCFG_TYPE_START]  = "KmppVencStartCfg",
     [MPP_VENC_KCFG_TYPE_STOP]   = "KmppVencStopCfg",
     [MPP_VENC_KCFG_TYPE_ST_CFG] = "KmppVencStCfg",
+    [MPP_VENC_KCFG_TYPE_REF_CFG] = "KmppVencRefCfg",
     [MPP_VENC_KCFG_TYPE_CTRL_CFG] = "KmppVencCtrlCfg",
 };
 static KmppObjDef kcfg_defs[MPP_VENC_KCFG_TYPE_BUTT] = {NULL};
